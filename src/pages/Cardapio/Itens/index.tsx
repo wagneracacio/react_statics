3e5540267361a93649a89227bdeabe9fbf2f31c7
Wagner <wmaluk@gmail.com>
2022-07-21 19:50:18 -0300

    add itens

---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,16 @@
+import Item from "./Item";
+import cardapio from "./itens.json";
+import styles from "./Itens.module.scss";
+
+export default function Itens() {
+  return (
+    <div className={styles.itens}>
+      {cardapio.map((item) => (
+        <Item 
+            key={item.id} 
+            {...item}
+        />
+      ))}
+    </div>
+  );
+}

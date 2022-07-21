38b795272c2ebdceb365281018e039ce76b4aa26
Wagner <wmaluk@gmail.com>
2022-07-21 11:21:39 -0300

    add normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Cardapio from './pages/Cardapio';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "normalize.css";
+import "./index.css";
+import Cardapio from "./pages/Cardapio";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>

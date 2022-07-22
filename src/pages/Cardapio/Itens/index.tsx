7cc2f04c6fbf84a941d65d964d0e540c7c69a702
Wagner <wmaluk@gmail.com>
2022-07-22 11:30:03 -0300

    add routes

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import Item from './Item'
-import cardapio from './itens.json'
+import cardapio from 'data/cardapio.json'
 import styles from './Itens.module.scss'
 
 interface Props {
